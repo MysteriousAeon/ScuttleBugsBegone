ade7523faddfa3b7ed34827df6411c5b01a58986
Okan Çetin <65574436+MysteriousAeon@users.noreply.github.com>
2023-02-27 04:46:29 +0300

    Add files via upload

---
+++ BugsBegone.cs
@@ -0,0 +1,44 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace ScuttleBugsBegone
+{
+    public class BugsBegone : MelonMod
+    {
+        private GameObject bugsGameObject;
+        private bool bugsGameObjectPresent = false;
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("ScuttleBugsBegone loaded!");
+        }
+        public override void OnUpdate()
+        {
+            // Check if the "Bugs" gameobject is present
+            if (bugsGameObject == null)
+            {
+                bugsGameObject = GameObject.Find("Bugs");
+                if (bugsGameObject == null)
+                {
+                    bugsGameObjectPresent = false;
+                    return;
+                }
+                else
+                {
+                    bugsGameObjectPresent = true;
+                }
+            }
+            else
+            {
+                bugsGameObjectPresent = true;
+            }
+
+            // Clean the grass if the "Bugs" gameobject is present
+            if (bugsGameObjectPresent)
+            {
+                Transform bug = GameObject.Find("Bugs").GetComponent<Transform>();
+                bug.localScale = new Vector3(-1000, -1000, -1000);
+                bug.forward = new Vector3(-1, -1, -1);
+            }
+        }
+    }
+}
