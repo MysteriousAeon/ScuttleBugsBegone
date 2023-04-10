d8814b6d74c698c0570371f2f6f49e7138afda98
Okan Çetin <65574436+MysteriousAeon@users.noreply.github.com>
2023-04-10 19:22:25 +0300

    Add files via upload
    New project files updating the mod to MelonLoader 0.6.1

---
+++ AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using MelonLoader;
+using ScuttleBugsBegone;
+
+[assembly: MelonInfo(typeof(BugsBegone), "ScuttleBugsBegone", "3.0.0", "MysteriousAeon & KomiksPL", "https://www.nexusmods.com/slimerancher2/mods/28")]
+[assembly: MelonGame("MonomiPark", "SlimeRancher2")]
---
+++ BugsBegone_.NET6.0.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ScuttleBugsBegone
+{
+    public class BugsBegone : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("ScuttleBugsBegone loaded!");
+            System.Action<Scene, LoadSceneMode> scneneLoaded = (s, s1) =>
+            {
+                if (s.name.Equals("GameCore"))
+                {
+                    var patchCoop = Resources.FindObjectsOfTypeAll<GameObject>().First(x => x.name.Equals("patchCoop"));
+                    var Bugs = patchCoop.transform.GetChild(4).GetChild(10).transform;
+                    Bugs.gameObject.SetActive(false);
+                    Bugs.transform.localScale = UnityEngine.Vector3.zero;
+                }
+            };
+            SceneManager.add_sceneLoaded(scneneLoaded);
+        }
+    }
+}
