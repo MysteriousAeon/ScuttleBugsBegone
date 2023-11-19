fd8d1f7a210cff7b7c84444e34b3da3dd14e2069
Okan Çetin <65574436+MysteriousAeon@users.noreply.github.com>
2023-11-19 05:02:55 +0300

    Add files via upload
    v3.0.0

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using MelonLoader;
 using ScuttleBugsBegone;
 
-[assembly: MelonInfo(typeof(BugsBegone), "ScuttleBugsBegone", "2.0.1", "MysteriousAeon & KomiksPL", "https://www.nexusmods.com/slimerancher2/mods/28")]
+[assembly: MelonInfo(typeof(BugsBegone), "ScuttleBugsBegone", "3.0.0", "MysteriousAeon & KomiksPL", "https://www.nexusmods.com/slimerancher2/mods/28")]
 [assembly: MelonGame("MonomiPark", "SlimeRancher2")]
